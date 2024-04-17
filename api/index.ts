3f9a88bb413fb67e7625925c32ae8429364faddc
Егор Матяс <play.filin@gmail.com>
2024-04-17 12:51:04 +0300

    feat: test vercel

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,39 @@
+// require('../src/index');
+
+// import express from 'express';
+// import { webhookCallback } from 'grammy';
+
+// import bot from '../src/core/bot';
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(`/api/index`, webhookCallback(bot));
+
+// export default app;
+
 require('../src/index');
 
-import express from 'express';
 import { webhookCallback } from 'grammy';
-
 import bot from '../src/core/bot';
 
-const app = express();
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, result => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
 
-app.use(express.json());
-app.use(`/api/index`, webhookCallback(bot));
+      return resolve(result);
+    });
+  });
+}
 
-export default app;
+async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, webhookCallback(bot));
+}
+
+export default handler;
